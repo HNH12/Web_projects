a431d081238ff11fc55eab4841b2ba972261ce1f
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-04-14 14:28:45 +0300

    При перемещении влево - задача выполнена. Вправо - удалена

--- scripts/start_script.js
+++ scripts/start_script.js
@@ -48,7 +48,23 @@ $(document).ready(() => {
     $('.add-task').click(() => {
         const taskInput = $('.task-input');
         const newTask = $(`<div class="task">${taskInput.val()}</div>`);
-        newTask.draggable({axis:"x"});
+        newTask.draggable(
+            {
+                axis: "x",
+                containment: [positionLeftTasksBlock, 0, positionRightTasksBlock, 0],
+                stop: function () {
+                    const absLeftBlock = $(this).position().left;
+                    console.log(absLeftBlock);
+                    if(absLeftBlock < (positionLeftTasksBlock/0.9)) {
+                        $(this).toggleClass('checked');
+                    } else {
+                        if(absLeftBlock > (positionRightTasksBlock/1.1)) {
+                            $(this).toggleClass('deleted');
+                        }
+                    }
+                }
+            }
+        );
         $('.tasks').append(newTask);
 
         taskInput.val('');
@@ -57,7 +73,23 @@ $(document).ready(() => {
     const widthTasksBlock = tasksBlock.width();
     const positionLeftTasksBlock = tasksBlock.position().left;
     const positionRightTasksBlock = widthTasksBlock-positionLeftTasksBlock;
-    console.log(positionLeftTasksBlock, positionRightTasksBlock);
 
-    $('.task').draggable({axis:"x", containment: [positionLeftTasksBlock,0, positionRightTasksBlock,0]});
+    const taskBlock = $('.task');
+    taskBlock.draggable(
+        {
+            axis: "x",
+            containment: [positionLeftTasksBlock, 0, positionRightTasksBlock, 0],
+            stop: function () {
+                const absLeftBlock = $(this).position().left;
+                console.log(absLeftBlock);
+                if(absLeftBlock < (positionLeftTasksBlock/0.9)) {
+                    $(this).toggleClass('checked');
+                } else {
+                    if(absLeftBlock > (positionRightTasksBlock/1.1)) {
+                        $(this).toggleClass('deleted');
+                    }
+                }
+            }
+        }
+    );
 });
